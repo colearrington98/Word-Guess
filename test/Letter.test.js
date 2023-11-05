32f3d9168eba65c80c0e632d8b29de18456dff7b
colearrington98@gmail.com <colearrington98@gmail.com>
2023-11-05 17:44:56 -0600

    Deleted test file

--- test/Letter.test.js
+++
@@ -1,7 +0,0 @@
-const Letter = require("../lib/Letter");
-
-describe("Letter", () => {
-    it('Character thata are digits or letters should not be visible', () => {
-        export(new Letter('j')).toBe(false);
-    });
-    
